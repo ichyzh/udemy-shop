a0cb7c321a04a25563660a7a49e4fe6aad5126f6
Illia CHYZH <ichyzh@e2r4p2.unit.ua>
2019-03-20 20:21:43 +0200

    add index view

--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -7,20 +7,9 @@ use ishop\Cache;
 class MainController extends AppController
 {
     public function indexAction() {
-        $posts = \R::findAll('test');
-        $posts = \R::findOne('test', 'id = ?', [2]);
+        $brands = \R::find('brand', 'LIMIT 3');
+        $hits = \R::find('product', "hit = '1' AND status = '1' LIMIT 8");
         $this->setMeta('Main Page', 'Description...', 'key');
-        $name = 'John';
-        $age = 30;
-        $names = ['Andrey', 'Jane', 'Mike'];
-        $cache = Cache::instance();
-        // $cache->set('test', $names);
-        $cache->delete('test');
-        $data = Cache::get('test');
-        if (!$data) {
-            $cache->set('test', $names);
-        }
-        debug($data);
-        $this->set(compact('name', 'age', 'names', 'posts'));
+        $this->set(compact('brands', 'hits'));
     }
 }
---
+++ application/widgets/currency/Currency.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace application\widgets\currency;
+
+class Currency
+{
+    protected $tpl;
+    protected $currencies;
+    protected $currency;
+
+    public function __construct() {
+        $this->tpl = __DIR__ . "/currency_tpl/currency.php";
+        $this->run();
+    }
+
+    protected function run() {
+
+    }
+
+    public static function getCurrencies() {
+
+    }
+
+    public static function getCurrency() {
+        
+    }
+
+    public function getHtml() {
+        
+    }
+}
+
